3cdb79b58c5bfed4d39eac76aa90a8f30ba53817
David Raynes <David.Raynes@teamaol.com>
2016-02-03 19:10:08 -0500

    FizzBuzz implementation

--- test/fizzbuzz.js
+++ test/fizzbuzz.js
@@ -9,25 +9,37 @@ describe('fizzBuzz', function() {
 
     // write a couple of your own tests
     // here's an example to get you started:
-    
+
+    it('should be [] for -1', function() {
+        fizzBuzz(-1).should.deep.equal([]);
+    });
+
     it('should be [] for 0', function() {
         fizzBuzz(0).should.deep.equal([]);
     });
 
+    it('should be ["FizzBuzz"] for 1', function() {
+        fizzBuzz(1).should.deep.equal(['FizzBuzz']);
+    });
+
+    it('should be ["FizzBuzz", 1] for 2', function() {
+        fizzBuzz(2).should.deep.equal(['FizzBuzz', 1]);
+    })
+
     // when you are feeling confident, uncomment the following tests
     // that are based on the function description
-    
-    // describe('examples from the function description', function() {
-    //     [
-    //         [0, []],
-    //         [2, ['FizzBuzz', 1]],
-    //         [7, ['FizzBuzz', 1, 2, 'Fizz', 4, 'Buzz', 'Fizz']],
-    //         [16, ['FizzBuzz', 1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']]
-    //     ].forEach(function(testValues) {
-    //           it('fizzBuzz(' + testValues[0] + ')', function() {
-    //               fizzBuzz(testValues[0]).should.deep.equal(testValues[1]);
-    //           });
-    //       });
-    // });
-       
+
+    describe('examples from the function description', function() {
+        [
+            [0, []],
+            [2, ['FizzBuzz', 1]],
+            [7, ['FizzBuzz', 1, 2, 'Fizz', 4, 'Buzz', 'Fizz']],
+            [16, ['FizzBuzz', 1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']]
+        ].forEach(function(testValues) {
+              it('fizzBuzz(' + testValues[0] + ')', function() {
+                  fizzBuzz(testValues[0]).should.deep.equal(testValues[1]);
+              });
+          });
+    });
+
 });

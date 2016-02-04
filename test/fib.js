979b2467bde4e1e32fc1f83726d76111b27e5fa0
Mike Ogrysko <michael.ogrysko@comcast.net>
2016-02-03 20:51:30 -0500

    fin implementation

--- test/fib.js
+++ test/fib.js
@@ -14,23 +14,38 @@ describe('fib', function() {
         fib(0).should.equal(0);
     });
 
+    it('should be 1 for 1', function() {
+        fib(1).should.equal(1);
+    });
+
+    it('should be 1 for 2', function() {
+        fib(2).should.equal(1);
+    });
 
     // when you are feeling confident, uncomment the following tests
     // that are based on the function description
 
-    // describe('examples from the function description', function() {
-    //     [
-    //         [0, 0],
-    //         [2, 1],
-    //         [7, 13],
-    //         [16, 987]
-    //     ].forEach(function(testValues) {
-    //           it('fib(' + testValues[0] + ')', function() {
-    //               fib(testValues[0]).should.equal(testValues[1]);
-    //           });
-    //       });
-    // });
+    describe('examples from the function description', function() {
+        [
+            [0, 0],
+            [2, 1],
+            [7, 13],
+            [16, 987]
+        ].forEach(function(testValues) {
+              it('fib(' + testValues[0] + ')', function() {
+                  fib(testValues[0]).should.equal(testValues[1]);
+              });
+          });
+    });
 
     // feeling brave? try writing a test for 25 or 50
+
+    it('should be 75025 for 25', function() {
+        fib(25).should.equal(75025);
+    });
+
+    it('should be 832040 for 30', function() {
+        fib(30).should.equal(832040);
+    });
 
 });

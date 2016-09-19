c26a007f11541522df476a28e623d81e3e7a5e64
David Raynes <rayners@gmail.com>
2016-09-18 21:29:13 -0400

    Update based on in-class solutions

--- test/fib.js
+++ test/fib.js
@@ -14,22 +14,42 @@ describe('fib', function() {
         fib(0).should.equal(0);
     });
 
+    it('should be 1 for 1', function() {
+        fib(1).should.equal(1);
+    });
+
+    it('should be 1 for 2', function() {
+        fib(2).should.equal(1);
+    });
+
+    it('should be 2 for 3', function() {
+        fib(3).should.equal(2);
+    });
+
     // when you are feeling confident, uncomment the following tests
     // that are based on the function description
-    
-    // describe('examples from the function description', function() {
-    //     [
-    //         [0, 0],
-    //         [2, 1],
-    //         [7, 13],
-    //         [16, 987]
-    //     ].forEach(function(testValues) {
-    //           it('fib(' + testValues[0] + ')', function() {
-    //               fib(testValues[0]).should.equal(testValues[1]);
-    //           });
-    //       });
-    // });
+
+    describe('examples from the function description', function() {
+        [
+            [0, 0],
+            [2, 1],
+            [7, 13],
+            [16, 987]
+        ].forEach(function(testValues) {
+              it('fib(' + testValues[0] + ')', function() {
+                  fib(testValues[0]).should.equal(testValues[1]);
+              });
+          });
+    });
 
     // feeling brave? try writing a test for 25 or 50
-    
+
+    it('should be 75025 for 25', function() {
+        fib(25).should.equal(75025);
+    });
+
+    it('should be 12586269025 for 50', function() {
+        fib(50).should.equal(12586269025);
+    });
+
 });
